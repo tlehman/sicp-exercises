3c0bcce1da0fc40f9fd68d6628efeb6907c412ec
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-01 14:18:21 -0800

    Do 1.4 and 1.5

--- ex-1-4.scm
+++ ex-1-4.scm
@@ -1,1 +1,9 @@
-; Exercise 1.4: Observe that our model of computation allows for combinations whose operators are compound expressions
+; Exercise 1.4: Observe that our model of computation allows for combinations whose operators are compound expressions.
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -)) a b)
+; => a-plus-abs-b
+
+(a-plus-abs-b 24 -7)  ; FIX THIS
+
+
---
+++ ex-1-5.scm
@@ -0,0 +1,16 @@
+; Exercise 1.5: There is a test to determine whether the interpreterone is faced with is using applicative-order evaluation or normal-order evaluation. The following two procedures:
+
+(define (p) (p))
+(define (test x y)
+  (if (= x 0) 0 y))
+
+; Then, when the expression:
+(test 0 (p))
+; is evaluated, what will happen
+;  with Applicative order evaluation, (test 0 (p)) would evaluate
+					;   0 and then (p), which would evaluate to (p), 
+					;   which would evaluate to (p) ... ad infinitum
+
+;  with Normal order evaluation, test would be evaluated first, passing in 0 and (p), and since
+					; (= 0 0) evaluates to #t, the entire expression would 
+					; evaluate to 0

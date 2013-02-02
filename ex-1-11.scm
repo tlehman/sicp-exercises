86d84347d95a90e2986e0698636420d30ac1f1c1
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-01 19:37:19 -0800

    Add iterative fibonacci definition

--- ex-1-11.scm
+++ ex-1-11.scm
@@ -7,7 +7,16 @@
 	(else (+ (fib (- n 1))
 		 (fib (- n 2))))))
 
+(define (fib n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+	b
+	(fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+
 
 ; (map fib '(0 1 2 3 4 5 6 7 8))
 ; => (0 1 1 2 3 5 8 13 21)
+
+
 

c4ca66c37fe05b22c7dc1c885e3dc65eda690425
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-01 18:58:57 -0800

    Finish all Ackerman special cases, except h. What is h?

--- ex-1-10.scm
+++ ex-1-10.scm
@@ -6,3 +6,22 @@
 	((= y 1) 2)
 	(else (A (- x 1)
 		 (A x (- y 1))))))
+
+(A 1 10)
+; => 1024
+
+(A 2 4)
+; => 65536
+
+(A 3 3)
+; => 65536
+
+; Consider the following procedures, where A is the procedure defined above: 
+(define (f n) (A 0 n))     ; 2n
+(define (g n) (A 1 n))     ; 2^n
+(define (h n) (A 2 n))     ; (0 2 4 16 65536)  
+(define (k n) (* 5 n n))   ; 5n^2
+
+; Give concise mathematical definitions for the functions computed by the procedures f,g, and h for positive integer values of n. For example, (k n) computes 5n^2
+; ^^ Given in the comments ^^
+

296c2c7982719c00dfa99e8c2d12e9817a9b272f
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-03 11:09:48 -0800

    Fix base case bug in pascal procedure, and add test at bottom of ex-1-12.scm file

--- ex-1-12.scm
+++ ex-1-12.scm
@@ -12,10 +12,17 @@
 (define (pascal row)        ; pascal maps row |--> next-row
   (define (pascal-cdr row)
     (if (< (length row) 2)
-	'(1)
+	(if (= (length row) 0)
+	    '()
+	    '(1))
 	(cons (+ (car row) (car (cdr row)))
 	      (pascal-cdr (cdr row)))))
 
   (cons 1 (pascal-cdr row)))
 
 
+(pascal '())                                      ; => (1)
+(pascal (pascal '()))                             ; => (1 1)
+(pascal (pascal (pascal '())))                    ; => (1 2 1)
+(pascal (pascal (pascal (pascal '()))))           ; => (1 3 3 1)
+(pascal (pascal (pascal (pascal (pascal '())))))  ; => (1 4 6 4 1)

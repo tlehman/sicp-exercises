093dad34067931203e8acb8ad5a5b4b5f56fd807
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-02 19:41:04 -0800

    Remove broken sqrt definition

--- 1_newtons_method.scm
+++ 1_newtons_method.scm
@@ -20,20 +20,3 @@
 
 (define (improve guess x)
   (average guess (/ x guess)))
-
-;; Exercise 1.6
-;; What happens if one defines new-if in terms of cond, 
-;; and then uses that new-if in the sqrt function?
-(define (new-if predicate then-clause else-clause)
-  (cond (predicate then-clause)
-	(else else-clause)))
-
-(define (sqrt-iter guess x)
-  (new-if (good-enough? guess x) guess
-      (sqrt-iter (improve guess x) x)))
-;Aborting!: maximum recursion depth exceeded
-;; 
-;; The reason this happens is that ordinary functions in Scheme
-;; use applicative-order evaluation, so the operands are computed 
-;; first, which never terminates, due to the recursion being used 
-;; in an incorrect way. Therefore, the special form IF must exist.

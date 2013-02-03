2f4de8f4035e384148954ba105739a23ec7a72d5
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-02 20:32:23 -0800

    Exercise 1.11: Add iterative version of f

--- ex-1-11.scm
+++ ex-1-11.scm
@@ -4,42 +4,25 @@
 (define (f n) 
   (if (< n 3)
       n
-      (+ (f (- n 1)) (* 2 (- n 2)) (* 3 (- n 3)))))  ; Find solution to linear recurrence analytically
+      (+ (f (- n 1)) 
+	 (* 2 (f (- n 2))) 
+	 (* 3 (f (- n 3))))))  ; Find solution to linear recurrence analytically
 
-;(map f '(1 2 3 4 5 6 7 8))
-; => (1 2 4 11 23 40 62 89)
-
-(define (f n)
-  (define (f-iter a b c count)
-    (if (= count 0)
-	c
-	(f-iter a b c (- count 1))))   ;; FIX
-    
-  (if (< n 3)
-      n
-      (f-iter a b c (- count)
-      
-;
 ; (map f '(0 1 2 3 4 5 6 7 8 9))
-; => (0 1 2 4 11 23 40 62 89 121)
+; => (0 1 2 4 11 25 59 142 335 796)
+; 
 
+; f(n) = f(n-1) + 2f(n-2) + 3f(n-3)
+; 
+;  a <- b
+;  b <- c 
+;  c <- c + 2b + 3a
 
-;  the Fibonacci numbers, computed tree-recursively:
-
-(define (fib n)
-  (cond ((= n 0) 0)
-	((= n 1) 1)
-	(else (+ (fib (- n 1))
-		 (fib (- n 2))))))
-
-(define (fib n)
-  (define (fib-iter a b count)
+(define (g n)
+  (define (iter a b c count)
     (if (= count 0)
-	b
-	(fib-iter (+ a b) a (- count 1))))
-  (fib-iter 1 0 n))
-
-
-; (map fib '(0 1 2 3 4 5 6 7 8))
-; => (0 1 1 2 3 5 8 13 21)
+	a
+	(iter b c (+ c (* 2 b) (* 3 a)) (- count 1))))
+  
+  (iter 0 1 2 n))
 

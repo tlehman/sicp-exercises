99e2c7245f99a90215bd9e04d45c23c1e6fb8f19
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-08 08:27:40 -0800

    annotate count-change function

--- count-change.scm
+++ count-change.scm
@@ -6,15 +6,18 @@
 (define (cc amount kinds-of-coins)
   (cond ((= amount 0) 1)
 	((or (< amount 0) (= kinds-of-coins 0)) 0)
-	(else (+ (cc amount
-		     (- kinds-of-coins 1))
+	(else (+ (cc amount             
+		     (- kinds-of-coins 1)) ; the # of ways to change a using all but the first kind of coin
 		 (cc (- amount
 			(first-denomination kinds-of-coins))
-		     kinds-of-coins)))))
+		     kinds-of-coins) ; the # of ways to change a-d where d is the first denomination
+		 ))))
 
+; first-denomination takes the number of kinds of coins and returns the denomination of the first kind
 (define (first-denomination kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
 	((= kinds-of-coins 2) 5)
 	((= kinds-of-coins 3) 10)
 	((= kinds-of-coins 4) 25)
 	((= kinds-of-coins 5) 50)))
+

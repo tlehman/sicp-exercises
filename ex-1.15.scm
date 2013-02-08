1446b145095063fee2220992097df5f191d4311d
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-08 08:49:20 -0800

    finish 1.15a

--- ex-1.15.scm
+++ ex-1.15.scm
@@ -2,10 +2,32 @@
 ;  
 ;  sin(x) = 3sin(x/3) - 4sin^3(x/3)
 ;
-(define (mysin x) 
-  (- (* 3 (sin (/ x 3)))
-     (* 4 
-	(* (sin (/ x 3)) 
-	   (sin (/ x 3)) 
-	   (sin (/ x 3))) )))
+;; (define (mysin x) 
+;;   (- (* 3 (sin (/ x 3)))
+;;      (* 4 
+;; 	(* (sin (/ x 3)) 
+;; 	   (sin (/ x 3)) 
+;; 	   (sin (/ x 3))) )))
+
 ; For the purposes of this exercise, "sufficiently small" is any angle less than 0.1 radians.
+(define p-applied 0)
+
+(define (cube x) (* x x x))
+
+(define (p x) 
+  (begin 
+    (set! p-applied (+ p-applied 1))
+    (- (* 3 x) (* 4 (cube x)))
+    )
+)
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+; (a) How many times is the procedure p applied when (sine 12.15) is evaluated?
+(display p-applied)
+; => 5    ; <---- five times
+
+

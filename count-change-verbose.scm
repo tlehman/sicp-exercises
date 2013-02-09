a9aaed48ca23cae18bfa5b5de75e6257f55f9519
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-08 21:41:14 -0800

    Add print statement to get a feel for the flow

---
+++ count-change-verbose.scm
@@ -0,0 +1,31 @@
+; Count Change
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (= kinds-of-coins 0))
+	 (begin 
+	   (display "\n")
+	   0))
+	(else (+ (begin
+		   (display `(cc ,amount ,kinds-of-coins))
+		   (display "->")
+		   (cc amount (- kinds-of-coins 1))) ; the # of ways to change a using all but the first kind of coin
+		 (begin
+		   (display `(cc ,amount ,(first-denomination kinds-of-coins)))
+		   (display "->")
+		   (cc (- amount
+			  (first-denomination kinds-of-coins))
+		       kinds-of-coins)) ; the # of ways to change a-d where d is the first denomination
+		 ))))
+
+; first-denomination takes the number of kinds of coins and returns the denomination of the first kind
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+	((= kinds-of-coins 2) 5)
+	((= kinds-of-coins 3) 10)
+	((= kinds-of-coins 4) 25)
+	((= kinds-of-coins 5) 50)))
+

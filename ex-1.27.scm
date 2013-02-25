30692be5b42a3b6f5ad8fc8cb93425fcd74fc746
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-24 18:27:12 -0800

    Add exercises 28 and 29

---
+++ ex-1.27.scm
@@ -0,0 +1,4 @@
+; Exercise 1.27: Demonstrate that Carmichael numbers listed in Footnote 1.47 really do fool the Fermat test. That is, write a procedure that takes an integer n and tests whether a^n is congruent to (a mod n) for every a < n, and try your procedure on the given Carmichael numbers.
+
+; TODO: Finish this
+

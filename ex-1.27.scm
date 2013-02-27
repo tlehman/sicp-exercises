b31a3fe40b3fd3f0b8c2a80cdd4a2fcd73c4217a
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-26 20:22:59 -0800

    Finish 1.27

--- ex-1.27.scm
+++ ex-1.27.scm
@@ -1,4 +1,27 @@
 ; Exercise 1.27: Demonstrate that Carmichael numbers listed in Footnote 1.47 really do fool the Fermat test. That is, write a procedure that takes an integer n and tests whether a^n is congruent to (a mod n) for every a < n, and try your procedure on the given Carmichael numbers.
 
-; TODO: Finish this
+; Fermat's Little Theorem: If n is a prime number and a is any positive integer less than n, then a raised to the n-th power is congruent a modulo n
 
+;           (n is prime)  implies    (a^n mod n = a (mod n))
+; (a^n mod n = a (mod n))  does not imply   (n is prime)
+
+; 561, 1105, 1729, 2465, 2821, 6601 are Carmichael numbers
+
+(load "helpers.scm")
+
+(define test-numbers '(561 1105 1729 2465 2821 6601))
+
+(define (carmichael? n)
+  (define (fermat-test a n)
+    (= (expmod a n n) a))
+
+  (define (try-fermat-test a n)
+    (if (= a n) #t
+	(and 
+	 (fermat-test a n)
+	 (try-fermat-test (+ a 1) n))))
+
+  (try-fermat-test 1 n))
+
+;(map carmichael? test-numbers)
+;Value 2: (#t #t #t #t #t #t)
--- helpers.scm
+++ helpers.scm
@@ -14,3 +14,15 @@
   (find-divisor n 2))
 
 (define (prime? n) (= n (smallest-divisor n)))
+
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp) 
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else 
+	 (remainder (* base (expmod base (- exp 1) m))
+		    m))))
+
+

fa4de67bc00f10992999a708989c76d03b176ab6
Tobi Lehman <tobi.lehman@gmail.com>
2013-03-07 11:32:42 -0800

    Working on 31

--- ex-1.31.scm
+++ ex-1.31.scm
@@ -11,14 +11,20 @@
 (define (factorial n)
   (product id 1 inc n))
 
-(display (factorial 5))
+(display (factorial 3))
 
 ; Also use product to compute approximations to π using the formula:
 ;
-;   π   2*4*4*6*6*8...
-;   - = --------------
-;   4   3*3*5*5*7*7...
-;   
-;
-; TODO: Finish this
+;   π   2*4*4*6*6*8...  2 4 4 6 6 8 ...  n(n+2)
+;   - = --------------  - - - - - - ---  -------
+;   4   3*3*5*5*7*7...  3 3 5 5 7 7 ...  (n+1)^2
+
+(load "helpers.scm")
+
+(define (pi-approx steps)
+  (let ((f    (lambda (n) (/ (* n (+ n 2)) (* (+ n 1) (+ n 1)))))
+        (next (lambda (n) (+ n 2))))
+    (product f 2.0 next steps)))
+
+(display (pi-approx 3))
 

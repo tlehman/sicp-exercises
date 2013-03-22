b2223ec8e18ffa16da9711a29f17b4810f9291cb
Tobi Lehman <tobi.lehman@gmail.com>
2013-03-22 06:38:32 -0700

    Finish 1.35

--- ex-1.35.scm
+++ ex-1.35.scm
@@ -1,3 +1,21 @@
-; Exercise 1.35: 
+; Exercise 1.35: Show that the golden ratio φ is a fixed point
+;   of the transformation x ↦ 1+1/x, and use this face to 
+;   compute φ by means of the fixed-point prodcedure.
 
-; TODO: Finish this EOF
+(define tolerance 0.0001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+
+  (try first-guess))
+
+(display (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.00))
+; 1.61805555555556
+

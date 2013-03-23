1bb32bf7caf1c7ed45f46f3317ba32cfde62d739
Tobi Lehman <tobi.lehman@gmail.com>
2013-03-23 15:10:50 -0700

    Add 1.37 description

---
+++ ex-1.37.scm
@@ -0,0 +1,10 @@
+; Exercise 1.37: An infinite continued fraction is an expression of the form 
+;                  N₁
+;       f = ----------------
+;            D₁   +    N₂
+;                   --------
+;                   D₂ + N₃
+;                        ___
+;                        D₃… 
+
+ ; TODO: Finish this EOF

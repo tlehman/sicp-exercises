5fd2b171b0ca15c0d083ba65a962a014193e79e9
Tobi Lehman <tobi.lehman@gmail.com>
2013-03-28 19:15:30 -0700

    Add 1.45

--- ex-1.45.scm
+++ ex-1.45.scm
@@ -1,3 +1,10 @@
-; Exercise 1.45: 
+; Exercise 1.45: We saw in Section 1.3.3 that attempting to compute square roots by naively 
+; finding a fixed point of y ↦ x/y does not converge, and that this can be fixed by average 
+; damping. The same method works for finding cube roots as fixed points of the average-
+; damped y ↦ x/y².
+;
+; Unfortunately, the process does not work for fourth roots - a single average damp is not 
+; enough to make a fixed point search for y ↦ x/y³
+
 
  ; TODO: Finish this EOF

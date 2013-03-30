d6444ea64c53a4e8ea2b62e068978ffb62b28c2e
Tobi Lehman <tobi.lehman@gmail.com>
2013-03-30 14:47:50 -0700

    Go back and start 1.08

---
+++ ex-1.08.scm
@@ -0,0 +1,11 @@
+; Exercise 1.08: Newton's method for cube roots is based on the fact
+; that if y is an approximation to the cube root of x, then a better
+; approximation is given by the value 
+; 
+;       (x/y² + 2y)
+;       -----------
+;            3
+
+
+
+ ; TODO: Finish this EOF

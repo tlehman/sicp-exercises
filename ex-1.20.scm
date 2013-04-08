c312a7b68b873ada623aef0f6b1ce364750d63eb
Tobi Lehman <tobi.lehman@gmail.com>
2013-04-07 21:40:12 -0700

    Worked on 1.20 using quasiquoting

--- ex-1.20.scm
+++ ex-1.20.scm
@@ -1,17 +1,28 @@
-; Exercise 1.20: The process that a procedure generates is of course dependent on the rules used by the interpreter. As an example, consider the iterative gcd procedure:
+; Exercise 1.20: The process that a procedure generates is of course
+; dependent on the rules used by the interpreter. 
+; 
+; As an example, consider the iterative gcd procedure:
 
 (define (gcd a b)
   (if (= b 0)
-      a
-      (gcd b (remainder a b))))
+       a
+       `(gcd ,b ,(remainder a b))))
 
 ; In normal order evaluation, use the substitution method to illustrate the execution of:
 (gcd 206 40)
+(gcd 40 (remainder 206 40))
+(gcd 6 (remainder 40 6))
+(gcd 4 (remainder 6 4))
+(gcd 2 (remainder 4 2))
+2
 
-; Do the same for applicative order evaluation, compare the number of times remainder is called.
+; Do the same for applicative order evaluation, compare the number of
+; times remainder is called.
+(gcd 206 40)
+(gcd 40 6)
+(gcd 6 4)
+(gcd 4 2)
+(gcd 2 0)
+2
 
-; TODO: Use macros to simulate normal-order evaluation
-
-
-
-
+; TODO: Check if correct

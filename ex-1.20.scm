6c50abed58af5371646cf4709dd8f2ce6f11446b
Tobi Lehman <tobi.lehman@gmail.com>
2013-04-14 18:28:42 -0700

    Finish 1.20

--- ex-1.20.scm
+++ ex-1.20.scm
@@ -3,18 +3,33 @@
 ; 
 ; As an example, consider the iterative gcd procedure:
 
+;(define (remainder a b) `(remainder ,a ,b))
+
 (define (gcd a b)
-  (if (= b 0)
+  (if (and (number? b) (= b 0))
        a
-       `(gcd ,b ,(remainder a b))))
+       `(gcd ,b (remainder ,a ,b))))
 
 ; In normal order evaluation, use the substitution method to illustrate the execution of:
 (gcd 206 40)
+
 (gcd 40 (remainder 206 40))
-(gcd 6 (remainder 40 6))
-(gcd 4 (remainder 6 4))
-(gcd 2 (remainder 4 2))
+
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+
+(gcd (remainder 40 (remainder 206 40)) 
+     (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 
+     (remainder (remainder 40 (remainder 206 40)) 
+		(remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+
+
 2
+
+;; remainder is called 11 times using normal-order evaluation
+
+
 
 ; Do the same for applicative order evaluation, compare the number of
 ; times remainder is called.
@@ -23,6 +38,8 @@
 (gcd 6 4)
 (gcd 4 2)
 (gcd 2 0)
+
 2
 
-; TODO: Check if correct
+;; remainder is called 5 times using normal-order evaluation
+

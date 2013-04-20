8bb17cd8a690c434ceb0ec7565325d6b783ef3b4
Tobi Lehman <tobi.lehman@gmail.com>
2013-04-20 11:25:25 -0700

    Clean up line length, fix matrix error

--- ex-1.19.scm
+++ ex-1.19.scm
@@ -1,19 +1,31 @@
-; Exercise 1.19: There is a clever algorithm for computing the Fibonacci numbers in a logarithmic number of steps. 
+; Exercise 1.19: There is a clever algorithm for computing the
+; Fibonacci numbers in a logarithmic number of steps.   
 ; 
-; Recall the transformation of the state variables a and b in the fib-iter process of Section 1.2.2: 
+; Recall the transformation of the state variables a and b in the
+; fib-iter process of Section 1.2.2:  
 ;   a <--- a + b
 ;   b <--- a
 ;
-; Call this transformation T, and observe that applying T over and over again n times, starting with 1 and 0, produces the pair Fib(n+1) and Fib(n). In other words, the Fibonacci numbers are produced by applying applying T^n to the pair (0,1).
+; Call this transformation T, and observe that applying T over and
+; over again n times, starting with 1 and 0, produces the pair
+; Fib(n+1) and Fib(n). In other words, the Fibonacci numbers are
+; produced by applying applying T^n to the pair (0,1). 
 ;
-; Now consider T to be the special case of p=0, q=1 in a family of transformations T_pq, where T_pq transforms the pair (a,b) according to:
+; Now consider T to be the special case of p=0, q=1 in a family of
+; transformations T_pq, where T_pq transforms the pair (a,b) according
+; to: 
 ;   a <--- bq + aq + ap
 ;   b <--- bp + aq
 ;
-; Show that if we apply such a transformation T_pq twice, the effect is the same as using a single transformation T_p'q' of the same form, and compute p' and q' in terms of p and q.
-; This gives us an explicit way to square these transformations, and thus we can compute T^n using successive squaring, as in the fast-expt procedure.
+; Show that if we apply such a transformation T_pq twice, the effect
+; is the same as using a single transformation T_p'q' of the same
+; form, and compute p' and q' in terms of p and q. 
+; This gives us an explicit way to square these transformations, and
+; thus we can compute T^n using succe ssive squaring, as in the
+; fast-expt procedure.
 ; 
-; Put this all together to complete the following procedure, which runs in a logarithmic number of steps:
+; Put this all together to complete the following procedure, which
+; runs in a logarithmic number of steps: 
 
 
 
@@ -43,7 +55,7 @@
 ; 
 ; Observe that the action of T_pq on (a,b) is linear, since T_pq(a,b)
 ;   a <--- bq + aq + ap = (p+q)a + qb
-;   b <--- bp + aq      = aa     + pb
+;   b <--- bp + aq      = qa     + pb
 ; Therefore, T_pq is a 2x2 matrix: 
 ;          
 ;  T_pq  =  | (p+q)  q |

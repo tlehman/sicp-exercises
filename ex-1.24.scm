6c06ae9fee1bc3f271515b4d92197d7703f2325b
Tobi Lehman <tobi.lehman@gmail.com>
2013-05-02 18:11:45 -0700

    work on 1.24

--- ex-1.24.scm
+++ ex-1.24.scm
@@ -2,6 +2,34 @@
 ; 1.22 to use fast-prime? (the Fermat method), and test each of the
 ; 12 primes you found in that exercise.
 
+(load "helpers.scm")
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else #f)))
+
+(load "ex-1.22.scm")
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 10)
+      (begin
+	(set! primes-found (+ primes-found 1))
+	(newline)
+	(display n)
+	(report-prime (- (runtime) start-time)))))
+
+(search-for-primes 100000000001 999999999999)
+
+;(search-for-primes 100000000000001 999999999999999)
+;(search-for-primes 100000000000000001 999999999999999999)
+
 
 
 			  

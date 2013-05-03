1e87cd9dece780bf8acfbdd66b483761464e0142
Tobi Lehman <tobi.lehman@gmail.com>
2013-05-02 21:50:34 -0700

    finish 2.01

--- ex-2.01.scm
+++ ex-2.01.scm
@@ -1,10 +1,16 @@
-; Exercise 2.01: Define a better version of make-rat that handles both
+; exercise 2.01: define a better version of make-rat that handles both
 ; positive and negative arguments. make-rat should normalize the sign
 ; so that if the rational number is postive, both the numerator and
 ; denominator are postive, and if the rational number is negative,
 ; only the numerator is negative.
 
-(define (make-rat n d) (cons n d))
+(define (make-rat n d)
+  (let ((g (gcd n d))
+	(s (/ (* n d) (abs (* n d)))))
+
+    (cons (* s (abs (/ n g)))
+	  (abs (/ d g)))))
+
 (define (numer x) (car x))
 (define (denom x) (cdr x))
 (define (print-rat x)
@@ -14,4 +20,9 @@
   (newline))
 
 
- ; TODO: Finish this EOF
+; gcd(a,b) = gcd(b,a%b)
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+

2ef55ddeeec2dc5b0f7c1dd619d61ccdd84e0b1b
Tobi Lehman <tobi.lehman@gmail.com>
2013-06-12 08:02:41 -0700

    start 2.33

---
+++ ex-2.33.scm
@@ -0,0 +1,11 @@
+; Exercise 2.33: Fill in the missing expressions to complete the
+; following definitions of some basic list-manipulation operations as
+; accumulations:
+
+(define (map p sequence)
+  (accumulate (lambda (x y) <??>) nil sequence))
+
+(define (length sequence)
+  (accumulate (lambda (x y) <??>) nil sequence))
+
+ ; TODO: Finish this EOF

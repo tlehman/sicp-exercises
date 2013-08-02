5976994d36c088ad3dac212d6f521be56762b50c
Tobi Lehman <tobi.lehman@gmail.com>
2013-08-01 22:49:34 -0700

    start 2.35

---
+++ ex-2.35.scm
@@ -0,0 +1,9 @@
+; Exercise 2.35: Redefine count-leaves from Section 2.2.2 as an
+; accumulation:
+(define (count-leaves t)
+  (accumulate (lambda (first rest)
+		)
+	      0
+	      t))
+
+ ; TODO: Finish this EOF

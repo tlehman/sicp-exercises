fa5a9d6e62dd04ea2785bb1098ab04bb44fa72f3
Tobi Lehman <tobi.lehman@gmail.com>
2013-08-01 22:46:12 -0700

    finish 2.34

--- ex-2.34.scm
+++ ex-2.34.scm
@@ -15,9 +15,30 @@
 ; a polynomial using Horner’s rule. Assume that the coefficients of
 ; the polynomial are arranged in a sequence, from a_0 through a_n.
 
+(load "ex-1.16.scm")  ; <--- for fast-expt
 
+; for this problem, we need an increasing integer, we will solve this 
+; problem with a functional iterator pow!
 
-(define (hornerl-eval x coefficient-sequence)
-  (accumulate (lamb
+(define (make-pow!)
+  (let ((pow 0))  ; local variable captured by closure below
+    (lambda ()
+      (let ((oldpow pow))
+	(begin
+	  (set! pow (+ pow 1))
+	  oldpow)))))  ; increment pow
 
- ; TODO: Finish this EOF
+(define pow! (make-pow!))
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coef higher-terms)
+		(if (eq? higher-terms '())
+		    0
+		    (* this-coef (fast-expt x (pow!)))))
+		0
+		coefficient-sequence))
+
+; For example, to compute 1 + 3x + 5x^2 + x^5 at x=2, you would eval:
+(horner-eval 2 '(1 3 0 5 0 1))
+; => 512
+

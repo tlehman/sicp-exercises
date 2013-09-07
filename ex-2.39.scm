67a234c36906fd6bc45fcf79c8c7ad2f126473a8
Tobi Lehman <tobi.lehman@gmail.com>
2013-09-07 09:35:22 -0700

    finish 2.39

---
+++ ex-2.39.scm
@@ -0,0 +1,17 @@
+; Exercise 2.39: Complete the following definitions of reverse in
+; terms of fold-left and fold-right from Exercise 2.38
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x)))
+	      nil
+	      sequence))
+
+; (reverse '(a b c d e))
+; => (e d c b a)
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+; (reverse '(a b c d e))
+; => (e d c b a)
+
